b3eeff57998fe1842a661f64e756148dbaabece1
ŁukaszMaterny <materny13@gmail.com>
2023-05-20 13:57:55 +0200

    CssAnimations

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,7 +2,8 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as standardTags } from './StandardTags/router-data';
+import { blockRouterMetaData as CssAnimations} from './CssAnimations/router-data';
 import { blockRouterMetaData as blockRouterMetaData120520231 } from './Blok10/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231,blockRouterMetaData120520231, standardTags];
+export const blockRouterMetaData = [blockRouterMetaData121220231,blockRouterMetaData120520231, standardTags, CssAnimations];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);

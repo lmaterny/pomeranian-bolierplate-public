36403fe3f9101d5a77312214ed1c6189145843fd
ŁukaszMaterny <materny13@gmail.com>
2023-05-21 13:21:47 +0200

    reszta tłumaczeń

---
+++ src/App/Exercises/Js/Exercise-js-booleans/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-function-booleans',
+  date: null,
+  linkLabel: 'Booleasn w JS',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['booleans', 'js', 'let', 'const'],
+};

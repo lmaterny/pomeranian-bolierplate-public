b3693e5f675a562d5d3dc142e207c762482a396e
ŁukaszMaterny <materny13@gmail.com>
2023-05-24 12:37:48 +0200

    box-model displaying emelents

---
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './styles.css';
+
+export function BoxModel() {
+  const { isTrue, setIsTrue } = useState(true);
+
+  const clickHandler = () => {
+    setIsTrue(!isTrue);
+  };
+  return (
+    <div>
+      <p className="paragraph" onClick={clickHandler}>
+        Donec eu eros commodo, malesuada nisi et, gravida ipsum. Nam quis
+        pretium est. Mauris convallis augue turpis, non imperdiet arcu feugiat
+        quis. Aenean nec aliquam justo, tincidunt imperdiet odio. Pellentesque
+        est nulla, ultrices in sem non, vestibulum suscipit lacus. Cras ac ex
+        nec lectus placerat vulputate. Proin sed lacus mi. Nulla molestie, elit
+        nec vehicula convallis, turpis enim tristique nisl, non auctor turpis
+        felis a magna. Praesent imperdiet augue id leo mollis, et tempor magna
+        rutrum. Donec vel odio ligula. Nam sem nisl, volutpat eget risus eget,
+        condimentum semper leo. Vivamus non luctus mi, sed auctor purus. Quisque
+        erat metus, interdum non semper at, pellentesque id ligula. Proin
+        venenatis tellus fringilla, posuere lacus ac, pellentesque augue.
+        Aliquam leo sapien, posuere finibus molestie nec, finibus id velit. Duis
+        porta pharetra mauris ac blandit.
+        {/*conditional rendering */}
+        {isTrue && <span className="inline-span">Test inlineowy</span>}
+      </p>
+    </div>
+  );
+}

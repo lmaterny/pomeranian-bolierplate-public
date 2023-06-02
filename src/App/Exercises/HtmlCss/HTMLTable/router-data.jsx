953d6149e2b99c71ba5d354c8a1cb749f37ca91f
ŁukaszMaterny <materny13@gmail.com>
2023-06-02 15:07:15 +0200

    dodane zadanie z tabelami

---
+++ src/App/Exercises/HtmlCss/HTMLTable/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { HTMLTable } from './index';
+
+export const blockRouterMetaData = {
+  path: '14-05-2023-2',
+  date: '14-05-2023',
+  linkLabel: 'Pierwsze tabelki',
+  blockNo: 16,
+  element: <HTMLTable />,
+  tags: ['tables', 'html', 'CSS'],
+};

c77655666c44d15816526721de355c9841a6caf5
ŁukaszMaterny <materny13@gmail.com>
2023-06-30 19:00:37 +0200

    Ćwiczenie formularza

---
+++ src/App/Exercises/ReactExercises/Forms/router-data.jsx
@@ -0,0 +1,10 @@
+import { Forms } from './Forms';
+import React from 'react';
+export const forms = {
+  path: 'Forms',
+  date: '30-06-2023',
+  linkLabel: 'Forms',
+  blockNo: 8,
+  element: <Forms />,
+  tags: ['forms', 'form', 'formularz', 'input', 'checkbox', 'textarea'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,6 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Blok8MetaData } from './Blok 8/router-data';
 import { Blok9MetaData } from './Blok 9/router-data';
+import { forms } from './Forms/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData, Blok8MetaData, Blok9MetaData];
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  Blok8MetaData,
+  Blok9MetaData,
+  forms,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
